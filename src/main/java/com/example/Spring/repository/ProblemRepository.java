de1d189cd0994adbd2c56618eec4d58f7fa01fb2
CharlesMuvaka <charles.uvaka@student.moringaschool.com>
2022-10-24 19:46:20 +0300

    create a repository interface that implements the JPA repo to use all methods involved in manipulating data in the database.

--- src/main/java/com/example/Spring/repository/ProblemRepository.java
+++ src/main/java/com/example/Spring/repository/ProblemRepository.java
@@ -1,8 +1,12 @@
-package com.example.Spring.models;
+package com.example.Spring.repository;
 
+import com.example.Spring.models.Problem;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 //JPARepository contains methods of creating, Reading, updating and Deleting resources from our database
+@Repository
 public interface ProblemRepository extends JpaRepository<Problem, Integer> {
+
 
 }
