35dd1e9b903e58ed5a88581c1fb05d7963b0de49
CharlesMuvaka <charles.uvaka@student.moringaschool.com>
2022-10-24 19:45:14 +0300

    inject the service Implementation class through auto-wiring and create a Post Api endpoint to add a Problem to the database

--- src/main/java/com/example/Spring/models/ProblemController.java
+++ src/main/java/com/example/Spring/models/ProblemController.java
@@ -1,16 +1,38 @@
 package com.example.Spring.models;
 
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.Spring.service.ProblemServiceImp;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.*;
 
-//@Controller - This annotation makes a Java class able to use
+//@Controller - This annotation makes a Java class able to handle http requests
 
 @RestController //Combination between the @controller and @ResponseBody annotation
-
+//@RequestMapping(path = "/api/v1")
 public class ProblemController {
 
-    @PostMapping(value = "/addProblem")
-    public void addProblem(){
+    @Value("${spring.datasource.password}")
+    private String databaseName;
 
+    @Autowired
+    private ProblemServiceImp service;
+
+
+//    @RequestMapping(value = "/addProblem", method = RequestMethod.POST) //generates an API endpoint
+//    @ResponseBody  //enables getting data from the server
+    @PostMapping(value = "/addProblem")
+    public Problem addProblem(@RequestBody Problem problem1){
+        System.out.println(problem1.getDuration());
+        service.addProblem(problem1);
+        return problem1;
+    }
+
+    //Sending data to the server - There are two ways
+    // 1. Using path Variables
+    // 2. Using request Params
+
+    @GetMapping("/getProblem/{id}/{name}")
+    public String getProblem(@PathVariable("id") int id, @PathVariable("name") String problemName){
+        return new String("Your problem id is " + id + "and its name is  " + problemName + "" + databaseName);
     }
 }
