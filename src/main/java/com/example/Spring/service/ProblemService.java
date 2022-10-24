783cc9e065ee9ebc94c7c34f3e551185bf0b742f
CharlesMuvaka <charles.uvaka@student.moringaschool.com>
2022-10-24 20:06:13 +0300

    create a method that retrieves all problems.

--- src/main/java/com/example/Spring/service/ProblemService.java
+++ src/main/java/com/example/Spring/service/ProblemService.java
@@ -2,7 +2,10 @@ package com.example.Spring.service;
 
 import com.example.Spring.models.Problem;
 
+import java.util.List;
+
 public interface ProblemService {
 
      Problem addProblem(Problem problem);
+     List<Problem> getProblems();
 }
