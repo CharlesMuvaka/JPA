dfe2d5d9b3e38fbb3a38049f3db58001beefd4d4
CharlesMuvaka <charles.uvaka@student.moringaschool.com>
2022-10-24 19:48:33 +0300

    create a class to manipulate data in the database.

---
+++ src/main/java/com/example/Spring/service/ProblemServiceImp.java
@@ -0,0 +1,11 @@
+package com.example.Spring.service;
+
+import com.example.Spring.models.Problem;
+import com.example.Spring.repository.ProblemRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProblemServiceImp implements ProblemService{
+    
+}
