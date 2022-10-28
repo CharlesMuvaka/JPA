5a5cbc0b65c44bf1b7a88ea0c0979ecab6d0ceaa
CharlesMuvaka <charles.uvaka@student.moringaschool.com>
2022-10-28 20:55:51 +0300

    add a method that to selects a problem entry from the database based on its name.

--- src/main/java/com/example/Spring/repository/ProblemRepository.java
+++ src/main/java/com/example/Spring/repository/ProblemRepository.java
@@ -4,9 +4,14 @@ import com.example.Spring.models.Problem;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 //JPARepository contains methods of creating, Reading, updating and Deleting resources from our database
 @Repository
 public interface ProblemRepository extends JpaRepository<Problem, Integer> {
+    //add a method to query the database based on the problems name
+    //SELECT * FROM user_problem WHERE name = :name;
+    Problem findByName(String name);
 
 
 }
