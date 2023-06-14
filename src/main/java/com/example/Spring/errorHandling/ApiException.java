43d0420998f9a3c10997f05b2ef3c73033d700e1
CharlesMuvaka <muvakacharles@gmail.com>
2023-06-14 16:17:23 +0300

    create an custom exception from springs ResponseEntityExceptionHandler class.

--- src/main/java/com/example/Spring/errorHandling/ApiException.java
+++ src/main/java/com/example/Spring/errorHandling/ApiException.java
@@ -1,15 +1,32 @@
 package com.example.Spring.errorHandling;
 
-public class ApiException extends RuntimeException{
-    private final int statusCode;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
-    public ApiException(String message, int statusCode) {
-        super(message);
-        this.statusCode = statusCode;
-    }
+@ControllerAdvice
+public class ApiException extends ResponseEntityExceptionHandler {
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        Map<String, Object> responseExceptionBody = new LinkedHashMap<>();
+        responseExceptionBody.put("timestamp", System.currentTimeMillis());
+        responseExceptionBody.put("status", status.value());
 
-    public int getStatusCode() {
-        return statusCode;
+        //Getting the response Errors
+        List<String> errors = ex.getBindingResult()
+                .getFieldErrors().stream().map(DefaultMessageSourceResolvable::getDefaultMessage).collect(Collectors.toList());
+
+        responseExceptionBody.put("errors", errors);
+        return new ResponseEntity<Object>(responseExceptionBody, status);
     }
 }
